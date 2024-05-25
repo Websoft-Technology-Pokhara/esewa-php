1e2041fd6af082469ee23c397cdcda965b78c1b1
Sagar Gurung <sgrgug@gmail.com>
2024-05-25 16:18:26 +0545

    add test folder

---
+++ tests/EsewaTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class EsewaTest extends TestCase
+{
+    public function testEsewa()
+    {
+        $this->assertTrue(true);
+    }
+}
